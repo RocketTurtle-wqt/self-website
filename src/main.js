969f6c2f932df351f0bab000761b6be006ef3f77
wqt <1600639146@qq.com>
2021-02-03 20:52:34 +0800

    初步选了基于vue的canvas背景组件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueParticlesBg from "particles-bg-vue";
+
+Vue.use(VueParticlesBg)
 
 Vue.config.productionTip = false
 
