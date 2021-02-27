7af58b3160e5639c2080037c20ba668514f6720a
wqt <1600639146@qq.com>
2021-02-27 23:23:39 +0800

    完成了抽取文章，下一步尝试应用websocket推送的功能

--- src/main.js
+++ src/main.js
@@ -6,13 +6,12 @@ import { Icon } from 'element-ui'
 import mavonEditor from 'mavon-editor'
 import SIdentify from './components/Identify'
 import 'mavon-editor/dist/css/index.css'
+import axios from 'axios'
 
 Vue.use(SIdentify)
-
 Vue.use(mavonEditor)
-
 Vue.use(Icon);
-
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Artical.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="artical">
+    <div class="markdown-body" v-html="artical"></div>
+  </div>
+</template>
+
+<script>
+import "mavon-editor/dist/css/index.css";
+
+export default {
+  name:"Artical",
+  data() {
+    return {
+      id:"20210227225811",
+      artical:""
+    }
+  },
+  methods:{
+    getArtical(){
+      this.$axios({
+        url:`http://127.0.0.1:7002/getartical`,
+        withCredentials:true,
+        method:'GET',
+        params:{
+          id
+        }
+      }).then(res=>{
+        console.log(res);
+      }).catch(err=>{
+        console.error(err);
+      });
+    }
+  },
+  mounted(){
+    this.$axios({
+      url:`http://127.0.0.1:7002/getartical`,
+      // withCredentials:true,
+      method:'GET',
+      params:{
+        id:this.id
+      }
+    }).then(res=>{
+      this.artical=res.data.artical;
+    }).catch(err=>{
+      console.error(err);
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
